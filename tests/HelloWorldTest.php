b4d161f101a31bc73625eb88f2d862146e2c1177
Masingita01 <30475102+Masingita01@users.noreply.github.com>
2018-08-28 22:56:08 +0200

    Update HelloWorldTest.php

--- tests/HelloWorldTest.php
+++ tests/HelloWorldTest.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @coversDefaultClass \MS35\WITS\HelloWorld
+ * @coversDefaultClass \MS35\WITS-SoftwareDesign\HelloWorld
  */
 class HelloWorldTest extends \PHPUnit_Framework_TestCase{
   protected $hello;
