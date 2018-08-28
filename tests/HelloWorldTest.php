7e912210ff7cc571eacd8f94adfba037e65398a0
Masingita01 <30475102+Masingita01@users.noreply.github.com>
2018-08-28 22:07:22 +0200

    added annotations and changed testWorld to testHelloWorld

--- tests/HelloWorldTest.php
+++ tests/HelloWorldTest.php
@@ -1,12 +1,17 @@
 <?php
+/**
+ * @coversDefaultClass \MS35\WITS-SoftwareDesign\HelloWorld
+ */
 class HelloWorldTest extends \PHPUnit_Framework_TestCase{
   protected $hello;
   
   public function setUp(){//this part of the code initiates the hello variable
     $this->hello = new \MS35\WITS-SoftwareDesign\HelloWorld();
   }
-  
-  public function testWorld(){//this part of the code checks if the value returned by the world() method is equal to word
+  /**
+    * @covers ::world
+    */
+  public function testHelloWorld(){//this part of the code checks if the value returned by the world() method is equal to word
     $this->assertSame('world',$this->hello->world());
   }
 }
