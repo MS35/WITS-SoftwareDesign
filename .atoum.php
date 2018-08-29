bab30300aaf69d17d787df12c25b86251c34a94c
Masingita01 <30475102+Masingita01@users.noreply.github.com>
2018-08-29 10:14:18 +0200

    reverted to previous version of atoum.php

--- .atoum.php
+++ .atoum.php
@@ -1,10 +1,24 @@
 <?php
+//.atoum.php
 
-  use mageekguy\atoum;
-  use mageekguy\atoum\reports;
+use mageekguy\atoum;
+use mageekguy\atoum\reports;
 
-  $coveralls = new reports\asynchronous\coveralls('src', 'myCoverallsProjectToken');
-  $coveralls->addWriter();
-  $runner->addReport($coveralls);
+$coveralls = new reports\asynchronous\coveralls('src', 'myCoverallsProjectToken');
+$defaultFinder = $coveralls->getBranchFinder();
+$coveralls
+        ->setBranchFinder(function() use ($defaultFinder) {
+                if (($branch = getenv('TRAVIS_BRANCH')) === false)
+                {
+                        $branch = $defaultFinder();
+                }
 
-  $script->addDefaultReport();
+                return $branch;
+        })
+        ->setServiceName(getenv('TRAVIS') ? 'travis-ci' : null)
+        ->setServiceJobId(getenv('TRAVIS_JOB_ID') ?: null)
+        ->addWriter()
+;
+$runner->addReport($coveralls);
+
+$script->addDefaultReport();
