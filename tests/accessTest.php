9bf5924049eaca1698784b80932fdbb01fbcf7c4
Masingita01 <30475102+Masingita01@users.noreply.github.com>
2018-09-15 23:27:00 +0200

    AssertNotNull added to accessTest.php

--- tests/accessTest.php
+++ tests/accessTest.php
@@ -14,6 +14,6 @@ class accessTest extends PHPUnit_Framework_TestCase
     public function testAccess(){
         //$this->assertSame('failed',$this->result->accessBad());
         $this->assertSame('failed',$this->result->accessGood());
-        $this->assertTrue(is_string($this->result->accessGood(), "Got a " . gettype($this->result->accessGood()) . " instead of a string");
+        $this->assertNotNull(is_string($this->result->accessGood(), "Got a " . gettype($this->result->accessGood()) . " instead of a string");
     }
 }
