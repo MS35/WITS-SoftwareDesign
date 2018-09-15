fa17035d1883d078be28809ee9275b3297851a4f
Masingita01 <30475102+Masingita01@users.noreply.github.com>
2018-09-15 07:28:34 +0200

    This does user login
    This differentiates whether the user is a lecturer,student or pimd worker.

---
+++ src/lectAcce.php
@@ -0,0 +1,61 @@
+<?php
+include 'databaseConnect.php';
+
+/* Select queries return a resultset */
+        class access{
+            public function accessGood(){
+                $args = func_get_args();
+                return $args[0];
+            }
+            public function accessBad(){
+                $args = func_get_args();
+                return $args[0];
+            }
+        }
+        $userid = $_POST["username"];
+        $password = $_POST["password"];
+        $con = new connect();//this is the class with the server connection details, so here you are writing the
+        $output = array();
+        //$row = array();
+        $success="";
+        if (isset($_POST['submit'])) {
+            if ($result = mysqli_query($con->connectDetail(), "SELECT * from users where user_id='$userid'")) {
+                while ($row = $result->fetch_assoc()) {
+                    $output[] = $row;//each json object is taken as a row in php
+                }
+                if (mysqli_num_rows($result)) {
+                    if ($output[0]["user_role"] == "LECTURER") {
+                        include 'Navigate.html';
+                        $success = "success";
+                        $acc = new access();
+                        $acc->accessGood($success);
+                    }
+                    if ($output[0]["user_role"] == "STUDENT") {
+                        //include 'Create.html';
+                        echo "student";
+                        $success = "success";
+                        $acc = new access();
+                        $acc->accessGood($success);
+                    }
+                    if ($output[0]["user_role"] == "PIMD") {
+                        echo "PIMD";
+                        $success = "success";
+                        $acc = new access();
+                        $acc->accessGood($success);
+                    }
+                } else {
+                    include 'Login.html';
+                    $failed = "failed";
+                    $acc = new access();
+                    $acc->accessBad($failed);
+                }
+            }
+        } else {
+            $failed = "failed";
+            //$acc = new access();
+            //$acc.access($failed);
+            //return $failed;
+            die();
+        }
+        $con->closeConnection();
+?>
