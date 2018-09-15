d38c8e4f84f8c5b4bf2333295ef260c7dbc9a082
Masingita01 <30475102+Masingita01@users.noreply.github.com>
2018-09-15 08:02:34 +0200

    Removed author name.

--- tests/accessTest.php
+++ tests/accessTest.php
@@ -1,11 +1,5 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: singi
- * Date: 2018/09/14
- * Time: 6:13 PM
- */
-/**
  * @coversDefaultClass \MS35\WitsSoftwareDesign\access
  */
 class accessTest extends PHPUnit_Framework_TestCase
