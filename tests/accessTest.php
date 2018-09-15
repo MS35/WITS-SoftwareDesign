21ef3627a8db9e2a417351b58d0aaf89fc6a491a
Masingita01 <30475102+Masingita01@users.noreply.github.com>
2018-09-15 07:24:21 +0200

    Access test
    This tests the user login php file

---
+++ tests/accessTest.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: singi
+ * Date: 2018/09/14
+ * Time: 6:13 PM
+ */
+/**
+ * @coversDefaultClass \MS35\WitsSoftwareDesign\access
+ */
+class accessTest extends PHPUnit_Framework_TestCase
+{
+    protected $result;
+    public function setUp(){
+        $this->result = new \MS35\WitsSoftwareDesign\access();
+    }
+
+    /**
+     * @covers ::accessGood
+     */
+    public function testAccess()
+    {
+        $this->assertSame('success',$this->result->accessGood());
+    }
+
+    /*
+     * @covers ::accessBad
+     */
+
+    public function accessBad(){
+        $this->assertSame('failed',$this->result->accessBad());
+    }
+}
