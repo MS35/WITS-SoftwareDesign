8cff6c7e1e0d1814ac33d7be6d41d7aa6471dd95
Masingita01 <30475102+Masingita01@users.noreply.github.com>
2018-09-27 16:27:52 +0200

    Added @covers

--- tests/AttemptBlockerTest.php
+++ tests/AttemptBlockerTest.php
@@ -1,21 +1,16 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: singi
- * Date: 2018/09/26
- * Time: 7:33 PM
- */
-
-/**
  * @coversDefaultClass \MS35\WitsSoftwareDesign\AttemptBlocker
  */
-class AttemptBlockerTest extends PHPUnit_Framework_TestCase
-{
+class AttemptBlockerTest extends \PHPUnit_Framework_TestCase{
     protected $result;
+    
     public function setup(){
         $this->result = new \MS35\WitsSoftwareDesign\AttemptBlocker();
     }
-
+    /*
+     * @covers ::confirmIPAddress
+     */
     public function testConfirmIPAddress()
     {
         $this->assertEquals(0,$this->result->confirmIPAddress);
