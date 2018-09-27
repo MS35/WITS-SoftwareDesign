44f1184fc742bc0a48d9d8c3d89232e51d3f0404
Masingita01 <30475102+Masingita01@users.noreply.github.com>
2018-09-27 12:58:08 +0200

    Update access.php

--- src/access.php
+++ src/access.php
@@ -1,63 +1,96 @@
 <?php
-namespace MS35\WitsSoftwareDesign;
-include 'connect.php';
-
+include_once('connect.php');
+include_once('getIpAddress.php');
+include_once('AttemptBlocker.php');
 /* Select queries return a resultset */
         class access{
-            public function accessBad($value){
-                //$args = func_get_args();
-                //return $args[0];
-                    if($value=="success"){
-                        return 'success';
-                    }
-                    else if($value=="failed"){
-                            return 'failed';
-                    }
+            public function accessGood(){
+                $args = func_get_args();
+                return $args[0];
             }
-        }
-        $con = new connect();//this is the class with the server connection details, so here you are writing the
-        $output = array();
-        //$row = array();
-        $success="";
-        if (isset($_POST['submit'])) {
-            $userid = $_POST["username"];
-            $password = $_POST["password"];
-            if ($result = mysqli_query($con->connectDetail(), "SELECT * from users where user_id='$userid'")) {
-                while ($row = $result->fetch_assoc()) {
-                    $output[] = $row;//each json object is taken as a row in php
-                }
-                if (mysqli_num_rows($result)) {
-                    if ($output[0]["user_role"] == "LECTURER") {
-                        include 'Navigate.html';
-                        $success = "success";
-                        $acc = new access();
-                        $acc->accessGood($success);
-                    }
-                    if ($output[0]["user_role"] == "STUDENT") {
-                        //include 'Create.html';
-                        echo "student";
-                        $success = "success";
-                        $acc = new access();
-                        $acc->accessGood($success);
-                    }
-                    if ($output[0]["user_role"] == "PIMD") {
-                        echo "PIMD";
-                        $success = "success";
-                        $acc = new access();
-                        $acc->accessGood($success);
+            public function accessBad(){
+                $args = func_get_args();
+                return $args[0];
+            }
+
+            /**
+             *
+             */
+            public function lec(){
+                $ip = new ip();
+                $ipString = $ip->getRealIPAddr();
+                //this shows the ip address
+                //echo $ip->getRealIPAddr();
+                //this gets the userid from the html form
+                $userid = $_POST["username"];
+                //this gets the password from the html form
+                $password = $_POST["password"];
+                //this is the class with the server connection details, so here you are writing the
+                $con = new connect();
+                //this array is going to take in the output from th database
+                $output = array();
+                //$row = array();
+                $success="";
+                if (isset($_POST['submit'])) {
+                    if ($result = mysqli_query($con->connectDetail(), "SELECT * from users where user_id='$userid'")) {
+                        while ($row = $result->fetch_assoc()) {
+                            $output[] = $row;//each json object is taken as a row in php
+                        }
+                        if (mysqli_num_rows($result)) {
+                            $atbl = new AttemptBlocker();
+                            if ($atbl->confirmIPAddress($ipString) == 1) {
+                                echo "Access denied for 2 minutes";
+                            } else if ($atbl->confirmIPAddress($ipString) == 0) {
+                                $atbl->clearLoginAttempts($ipString);
+                                if ($output[0]["user_role"] == "LECTURER") {
+                                    //this takes you to the navigate html page
+                                    $success = "success";
+                                    //this is from the class access
+                                    $acc = new lectAcce();
+                                    $acc->accessGood($success);
+                                    header("Location:Navigate.html");
+                                    exit();
+                                }
+                                if ($output[0]["user_role"] == "STUDENT") {
+                                    //include 'Create.html';
+                                    echo "student";
+                                    $success = "success";
+                                    $acc = new lectAcce();
+                                    $acc->accessGood($success);
+                                }
+                                if ($output[0]["user_role"] == "PIMD") {
+                                    echo "PIMD";
+                                    $success = "success";
+                                    $acc = new lectAcce();
+                                    $acc->accessGood($success);
+                                }
+                            }
+                        }
+                        else {
+                            $atbl = new AttemptBlocker();
+                            if($atbl->confirmIPAddress($ipString)==1){
+                                echo "Access denied for 2 minutes";
+                            }else if($atbl->confirmIPAddress($ipString)==0){
+                                echo "User name or password incorrect";
+                                $atbl->addLoginAttempt($ipString);
+                                header("Location:Login.html");
+                                exit();
+                            }
+                            $failed = "failed";
+                            $acc = new lectAcce();
+                            $acc->accessBad($failed);
+                        }
                     }
                 } else {
-                    include 'Login.html';
+
                     $failed = "failed";
-                    $acc = new access();
+                    $acc = new lectAcce();
                     $acc->accessBad($failed);
                 }
+                $con->closeConnection();
             }
-        } else {
-            $failed = "failed";
-            $acc = new access();
-            $acc->accessBad($failed);
-            //return $failed;
-            die();
         }
-        $con->closeConnection();
+        //object from
+        $lect = new access();
+        $lect->lec();
+?>
