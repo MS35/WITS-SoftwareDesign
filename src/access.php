aaf0caa671ecff936842c8e693dc66b3d1ec00fe
Masingita01 <30475102+Masingita01@users.noreply.github.com>
2018-09-28 02:13:38 +0200

    Update access.php

--- src/access.php
+++ src/access.php
@@ -18,52 +18,61 @@ include_once('AttemptBlocker.php');
              *
              */
             public function lec(){
+                session_start();
                 $ip = new ip();
                 $ipString = $ip->getRealIPAddr();
                 //this shows the ip address
-                //echo $ip->getRealIPAddr();
                 //this gets the userid from the html form
                 $userid = $_POST["username"];
+                //session variable which is sendable between php files 
+                $_SESSION["user_id"]=$userid;
                 //this gets the password from the html form
                 $password = $_POST["password"];
                 //this is the class with the server connection details, so here you are writing the
                 $con = new connect();
                 //this array is going to take in the output from th database
-                $output = array();
-                //$row = array();
+                $userID = array();
+                $userInfo = array();
                 $success="";
                 if (isset($_POST['submit'])) {
-                    if ($result = mysqli_query($con->connectDetail(), "SELECT * from users where user_id='$userid'")) {
+                    if ($result = mysqli_query($con->connectDetail(), "SELECT * from user_access where user_id='$userid' and password='$password'")) {
                         while ($row = $result->fetch_assoc()) {
-                            $output[] = $row;//each json object is taken as a row in php
+                            $userID[] = $row;//each json object is taken as a row in php
                         }
                         if (mysqli_num_rows($result)) {
-                            $atbl = new AttemptBlocker();
-                            if ($atbl->confirmIPAddress($ipString) == 1) {
-                                echo "Access denied for 2 minutes";
-                            } else if ($atbl->confirmIPAddress($ipString) == 0) {
-                                $atbl->clearLoginAttempts($ipString);
-                                if ($output[0]["user_role"] == "LECTURER") {
-                                    //this takes you to the navigate html page
-                                    $success = "success";
-                                    //this is from the class access
-                                    $acc = new access();
-                                    $acc->accessGood($success);
-                                    header("Location:Navigate.html");
-                                    exit();
+                            if($val = mysqli_query($con->connectDetail(),"SELECT * from users where user_id='$userid'")){
+                                while($useInf = $val->fetch_assoc()) {
+                                    $userInfo[] = $useInf;
                                 }
-                                if ($output[0]["user_role"] == "STUDENT") {
-                                    //include 'Create.html';
-                                    echo "student";
-                                    $success = "success";
-                                    $acc = new access();
-                                    $acc->accessGood($success);
-                                }
-                                if ($output[0]["user_role"] == "PIMD") {
-                                    echo "PIMD";
-                                    $success = "success";
-                                    $acc = new access();
-                                    $acc->accessGood($success);
+                            }
+                            if(mysqli_num_rows($val)){
+                                $atbl = new AttemptBlocker();
+                                if ($atbl->confirmIPAddress($ipString) == 1) {
+                                    echo "Access denied for 2 minutes";
+                                } else if ($atbl->confirmIPAddress($ipString) == 0) {
+                                    $atbl->clearLoginAttempts($ipString);
+                                    if ($userInfo[0]["user_role"] == "LECTURER") {
+                                        //this takes you to the navigate html page
+                                        $success = "success";
+                                        //this is from the class access
+                                        $acc = new lectAcce();
+                                        $acc->accessGood($success);
+                                        header("Location:Navigate.html");
+                                        exit();
+                                    }
+                                    if ($userInfo[0]["user_role"] == "STUDENT") {
+                                        //include 'Create.html';
+                                        echo "student";
+                                        $success = "success";
+                                        $acc = new lectAcce();
+                                        $acc->accessGood($success);
+                                    }
+                                    if ($userInfo[0]["user_role"] == "PIMD") {
+                                        echo "PIMD";
+                                        $success = "success";
+                                        $acc = new lectAcce();
+                                        $acc->accessGood($success);
+                                    }
                                 }
                             }
                         }
