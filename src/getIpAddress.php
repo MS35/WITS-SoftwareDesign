289e841335f3b1e216483b30fa14b9d859f4b69f
Masingita01 <30475102+Masingita01@users.noreply.github.com>
2018-10-11 22:05:09 +0200

    Update getIpAddress.php
    Un-commented  the remote addr

--- src/getIpAddress.php
+++ src/getIpAddress.php
@@ -14,7 +14,7 @@
             elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
                 $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
             } else {
-                //$ip = $_SERVER['REMOTE_ADDR'];
+                $ip = $_SERVER['REMOTE_ADDR'];
             }
             return $ip;
         }
