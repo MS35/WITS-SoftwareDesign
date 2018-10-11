9da7592410f4592210fbf29d19bbcca14cb5537d
Masingita01 <30475102+Masingita01@users.noreply.github.com>
2018-10-11 19:04:04 +0200

    Added password.

--- src/connect.php
+++ src/connect.php
@@ -6,8 +6,8 @@
         {
             $username = "s1312548";
             $password = "s1312548";
-            //$database = "d1312548";
-            $link = mysqli_connect("127.0.0.1", $username, $password);
+            $database = "d1312548";
+            $link = mysqli_connect("127.0.0.1", $username, $password, $database);
             return $link;
         }
         function closeConnection(){
