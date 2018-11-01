5f98f1e242c8a5b56fc9e61d84f7fabc8d4dfbb0
Masingita01 <30475102+Masingita01@users.noreply.github.com>
2018-11-01 19:58:00 +0200

    Added a test for tryCatch.py

---
+++ testTryCatch.py
@@ -0,0 +1,9 @@
+from nose.tools import *
+from TryCatch import *
+
+  class testTryCatch:
+    def testResult(0):
+      try1 = TryCatch(0)
+      try2 = TryCatch(1)
+      res = try.result(try2)
+      eq_(res,"ok")
